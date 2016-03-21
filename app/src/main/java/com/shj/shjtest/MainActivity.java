0115a90cb8ef7dbd4fff651ce0add255db497cb5
haojie <shihaojie@trans-it.cn>
2016-03-21 19:41:16 +0800

    add test

--- app/src/main/java/com/shj/shjtest/MainActivity.java
+++ app/src/main/java/com/shj/shjtest/MainActivity.java
@@ -2,21 +2,10 @@ package com.shj.shjtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-
-
-
-
-
-
-//sldkflksdl
 public class MainActivity extends AppCompatActivity {
     @Override
-    //sdfdf
     protected void onCreate(Bundle savedInstanceState) {
-        //sdfasd
         super.onCreate(savedInstanceState);
-        //sdfasdf
         setContentView(R.layout.activity_main);
 
     }
